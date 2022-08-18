e78ad2444c8e70fe954e48193dde133f06fba9eb
Laurajcb <callejaslaura96@gmail.com>
2022-08-18 15:43:50 -0500

    Fix Some styles and added logic to close and open icons

--- main.js
+++ main.js
@@ -1,39 +1,57 @@
 // Show  Email menu from email
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
-function toggleDesktopMenu() {
-  orderDetailMenu.classList.add('inactive');
-  desktopMenu.classList.toggle('inactive');
-}
 
 // show mobile menu from hamburguer icon 
 const menuHamburIcon = document.querySelector('.hambur-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-
 menuHamburIcon.addEventListener('click', toggleMobileMenu);
 
+// show order menu from cart icon
+const menuCartIcon = document.querySelector('.navbar-shopping-card');
+const orderedDetailMenu = document.querySelector('.my-order-detail');
+menuCartIcon.addEventListener('click', toggleOrderedDetailMenu);
+
+// Query for cards-container
+const cardsContainer = document.querySelector('.cards-container');
+// Query for product-detail container aside
+const orderDetailContainer = document.querySelector('.product-detail');
+
+//Query for product-detail-close
+const closeProductDetailIcon = document.querySelector('.product-datail-close');
+closeProductDetailIcon.addEventListener('click', closeProductDetail);
+
+
+function toggleDesktopMenu() {
+  orderedDetailMenu.classList.add('inactive');
+  orderDetailContainer.classList.add('inactive');
+  desktopMenu.classList.toggle('inactive');
+}
+
 function toggleMobileMenu() {
+  orderedDetailMenu.classList.add('inactive');
+  orderDetailContainer.classList.add('inactive');
   desktopMenu.classList.add('inactive');
   mobileMenu.classList.toggle('inactive');
 }
 
-// show order menu from cart icon
-const menuCartIcon = document.querySelector('.navbar-shopping-card');
-const orderDetailMenu = document.querySelector('.my-order-detail');
-
-menuCartIcon.addEventListener('click', toggleOrderDetailMenu);
-
-function toggleOrderDetailMenu() {
+//show order menu from cart icon
+function toggleOrderedDetailMenu() {
   mobileMenu.classList.add('inactive');
   desktopMenu.classList.add('inactive');
-  orderDetailMenu.classList.toggle('inactive');
+  orderDetailContainer.classList.add('inactive');
+  orderedDetailMenu.classList.toggle('inactive');
 }
 
-// query for cards-container
-const cardsContainer = document.querySelector('.cards-container');
+//function openProductDetailAside
+function openProductDetailAside() {
+  orderDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetail() {
+  orderDetailContainer.classList.add('inactive');
+}
 
 const productList = [];
 productList.push({
@@ -75,6 +93,7 @@ function renderProducts(arr) {
 
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.img);
+    productImg.addEventListener('click', openProductDetailAside);
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-basic-info');
